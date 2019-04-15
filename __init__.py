caecbe0db3758182da343b4f8b038549fa214548
RamVasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2019-04-15 16:35:33 -0400

    Use create generator functions using a nested generator function.

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from  diceware.diceware import *
-
-print(dice)
--- diceware_module.py
+++ diceware_module.py
@@ -5,6 +5,7 @@ class diceware_class(object):
 
     def __init__(self):
         self.word_list=self.read_word_list()
+        self.value_lsts={"$W":self.word_list,"$S":"!\"#%&'()*+,-./:;<=>?@[\\]^_`{|}~","$N":"0123456789"}
 
     def read_word_list(self,path: str="diceware/word_list/sindresorhus.txt")->list:
         word_lst = []
@@ -14,15 +15,15 @@ class diceware_class(object):
 
         return word_lst
 
-    def create_password(self,word_lst: list=None,template:str="$W$S$W$N$N$N$W$W")->str:
+    def create_password(self,word_lst: list=None,template:str="$W$N $W%N $W")->str:
         if word_lst is None:
             word_lst=self.read_word_list()
 
         password = template
 
-        password=self.replace(password,"$W",self.get_word)
-        password=self.replace(password,"$N",self.get_number)
-        password=self.replace(password,"$S",self.get_special_character)
+        password=self.replace(password,"$W",self.create_generator(self.word_list))
+        password=self.replace(password,"$N",self.create_generator("0123456789"))
+        password=self.replace(password,"$S",self.create_generator("$:!#%&'()*+,-./:;<=>?@[]^_`{|}~"))
         return password
 
     def get_word(self,word_lst:list=None):
@@ -43,7 +44,16 @@ class diceware_class(object):
             yield special_characters[randint(0,len(special_characters)-1)]
         return
 
-    def replace(self,template:str,old_substring:str,itr):
+    def create_generator(self,defult_lst):
+        def generator(lst=None):
+            if lst is None:
+                lst=defult_lst
+            while True:
+                yield lst[randint(0,len(lst)-1)]
+            return
+        return generator
+
+    def replace(self,template:str,old_substring:str,generator):
         while old_substring in template:
-            template=template.replace(old_substring,next(itr()),1)
+            template=template.replace(old_substring,next(generator()),1)
         return template
