448e961fb4d0a77578d24919631b5528dbb69804
Ram Vasu <36580682+RamVasuthevan@users.noreply.github.com>
2019-05-07 13:20:29 -0400

    -small fixes -adds password to clipboard

--- diceware/__main__.py
+++ diceware/__main__.py
@@ -1,11 +1,13 @@
 import sys
 import argparse
+import pyperclip
 from diceware_module import diceware_class
 
 
 def main():
     password_generator = diceware_class()
     #print(password_generator.create_password())
+    pyperclip.copy('The text to be copied to the clipboard.')
     print(password_generator.create_password())
 
 if __name__ == '__main__':
